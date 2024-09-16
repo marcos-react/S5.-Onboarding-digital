3237e7b44d81c3333ef61cdea9e450d59bfb5bce
evangelio7 <cristiano7@posteo.net>
2024-09-16 11:39:42 +0200

    Nivell 1 Exercici 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
+import timeManagment from './assets/time_managment.svg';
+import programming from './assets/programming.svg';
+import meditation from './assets/meditation.svg';
+
+
 
 const App = () => {
   const tutorialData = [
     {
       title: "Dedica moltes hores",
       description: "Un minim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li mes hores. Al principi sembla imposible, però notaràs una millora ràpidament",
-      bgcolor: "",
-      image: ""
+      bgcolor: "#44979f",
+      image: timeManagment
     },
     {
       title: "Programa projectes propis",
       description: "Mes val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivación i la implicació en el projecte ajudará a accelerar el teu aprenentatge",
-      bgcolor: "",
-      image: ""
+      bgcolor: "#ffffff",
+      image: programming
     },
     {
       title: "Procura descansar",
       description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduirás l'estrès i l'ansietat. Millorarás el teu aprenentatge",
-      bgcolor: "",
-      image: ""
+      bgcolor: "#bbbbbb",
+      image: meditation
     }
   ];
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import './Card.css'
 
 const Card = ({ title, description, bgcolor, image, nextStep}) => {
   return (
     <>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <div className = 'button'>
-        <button onClick={nextStep}>+</button>
-      </div>
+      <div className='card mx-auto'>
+        <div className = 'image justify-content-center align-items-center py-5' style={{ backgroundColor: bgcolor  }}>
+            <img src={image} alt={title} className='img-fluid py-5'></img>
+        </div>
+        <div className = 'card-box text-start py-2 px-3'>
+            <h1 className='fs-6'>{title}</h1>
+            <p className='fs-6'>{description}</p>
+  
+          {/* buttons */}
+            <div className='button d-flex justify-content-between align-items-center'>
+
+            {/* Indicators */}
+                <div className="indicators d-flex align-items-center">
+                {/* Short line */}
+                    <div className="line"></div>
+
+                {/* circle icon 1*/}
+                    <div className="circle"></div>
+
+                {/* circle icon 2 */}
+                    <div className="circle"></div>
+                </div>
+                    {/* Button arrow */}
+                    <button onClick={nextStep} className='btn btn-dark btn-circle rounded-circle d-flex justify-content-center align-items-center'>
+                        <i className='bi bi-arrow-right'></i>
+                    </button>
+            </div>
+        </div>
+       </div>
     </>
   );
 };
